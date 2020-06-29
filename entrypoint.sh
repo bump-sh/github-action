6a78ef68de3a360ebabd72e2d724965be417a1e4
Manfred Touron <94029+moul@users.noreply.github.com>
2020-06-29 17:15:43 +0200

    fix: use last CLI api
    context: the CLI API changed recently, this Github Action was broken.
    
    read more: https://github.com/bump-sh/github-action/pull/2
    
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
+# if you want to know why gem install is made in entrypoint.sh instead of Dockerfile,
+# please read the discussion here: https://github.com/bump-sh/github-action/pull/2.
 cd /home
 gem install bump-cli
-$GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --id $3 --token $4 --validation $5
+
+$GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --doc $3 --token $4 --validation $5
