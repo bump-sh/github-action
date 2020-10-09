10f9a6011eb7f6ad14ef4e295fd8e4f444146145
Sébastien Charrier <scharrier@gmail.com>
2020-10-09 21:59:08 +0200

    Import external references
    Activate the flag by default now that it has been tested by several
    users.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@
 cd /home
 gem install bump-cli
 
-$GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --doc $3 --token $4 --validation $5
+$GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --doc $3 --token $4 --validation $5 --import-external-references
