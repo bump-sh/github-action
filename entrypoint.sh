479ec6451123fd7fb7c9386db9603f6d497ff77e
Sébastien Charrier <scharrier@gmail.com>
2021-03-05 13:44:36 +0100

    Use bump-cli 0.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 # if you want to know why gem install is made in entrypoint.sh instead of Dockerfile,
 # please read the discussion here: https://github.com/bump-sh/github-action/pull/2.
 cd /home
-gem install bump-cli
+gem install bump-cli --version "~> 0.7"
 
 $GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --doc $3 --token $4 --validation $5
